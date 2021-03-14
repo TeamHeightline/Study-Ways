10bfeedd14175843bc368213f33562011b0a338a
Heightline <teamheightline@mail.ru>
2021-03-14 23:19:55 +0300

    add UnLogin page

--- src/Components/Login/UnLogin.tsx
+++ src/Components/Login/UnLogin.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react'
 // import 'react-bootstrap';
 import {Spinner} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 
 export default function UnLogin(){
+    const history = useHistory();
     localStorage.setItem('token', 'wrong key')
     localStorage.setItem('is_login', 'false')
     localStorage.setItem('user_name', '')
     localStorage.setItem('refreshToken', 'wrong refresh token')
-
+    setTimeout(()=>{
+        window.location.reload(false);
+    }, 1000)
+    setTimeout(history.push, 500, '/')
     return(
-        <div className="col-4 offset-4 justify-content-center mt-4">
+        <div className="col-1 offset-6 justify-content-center mt-4">
         <Spinner animation="border" variant="primary" />
         </div>
     )

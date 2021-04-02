52457bb4b2e1ed124626dd71209a09b77bc26daf
Heightline <teamheightline@mail.ru>
2021-04-02 09:48:38 +0300

    start dev Before test menu

--- src/Components/Experimental/BeforeTestMenu.tsx
+++ src/Components/Experimental/BeforeTestMenu.tsx
@@ -2,9 +2,18 @@ import React, {useState} from "react";
 import {gql, useQuery} from "@apollo/client";
 import {Button, Paper, TextField} from "@material-ui/core";
 import {Autocomplete} from "@material-ui/lab";
-import {Container, Form, Spinner} from "react-bootstrap";
+import {Accordion, Card, Container, Form, Spinner} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Table from "react-bootstrap/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import Checkbox from "@material-ui/core/Checkbox";
+import ReactPlayer from "react-player";
 
 const GET_ALL_QUESTIONS = gql`
 query GET_ALL_QUESTIONS{
@@ -14,23 +23,71 @@ query GET_ALL_QUESTIONS{
     
   }
 }`
-
+const GET_QUESTION_DATA = gql`
+      query GET_QUESTION_DATA($id: ID!) {
+            questionById(id: $id){
+              text
+              videoUrl
+              id
+              answers{
+                id
+                isTrue
+                text
+                helpTextv1
+                helpTextv2
+                helpTextv3
+                videoUrl
+                checkQueue
+                hardLevelOfAnswer
+              }
+            }
+      }
+    `
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
 
 export default function BeforeTestMenu (){
      const {data, error, loading, refetch} = useQuery(GET_ALL_QUESTIONS);
      const [helpLevel, changeHelpLevel] = useState("0");
      const [testHadBeenStarted, changeTestHadBeenStarted] = useState(false)
-
+    const [selectedQuestionId, changeSelectedQuestionId] = useState(-1)
+    const { data: get_question_data, loading: get_question_loading, error: get_question_error,
+        refetch: refetch_get_question } = useQuery(GET_QUESTION_DATA, { variables:
+            {
+                id: selectedQuestionId
+            }}
+    );
+    const classes = useStyles();
+    const [forRefresh, changeForRefresh] = useState(false)
+    const [selected, changeSelected] = useState<number[]>([])
     const onChangeHelpLevel = (event: any) => changeHelpLevel(event.target.value);
-     const autocompliteSelectHandleChange = (e : any, values: any) =>{
-         console.log(values)
-     }
-     if (!data){
-         return(
-             <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>
-         )
-     }
-     if (!testHadBeenStarted){
+    const autocompliteSelectHandleChange = (e : any, values: any) =>{
+         // console.log(values.id)
+         changeSelectedQuestionId(values.id)
+    }
+
+    function selectDeselectRow(id: any){
+        // console.log(id)
+
+        if( selected.indexOf(id) === -1) {
+            const oldSelected = selected
+            oldSelected.push(id)
+            // console.log(oldSelected)
+            changeSelected(oldSelected)
+        }
+        else{
+            const oldSelected = selected
+            oldSelected.splice(selected.indexOf(id), 1)
+            changeSelected(oldSelected)
+        }
+    }
+
+    const checkurl=(url: any)=>url.replace("http://","").replace("https://","").replace("www.","").replace("youtu.be/","youtube.com?v=").slice(0,14)==="youtube.com?v=";
+
+    if (!testHadBeenStarted){
          return(
              <div>
                  <Container>
@@ -73,9 +130,64 @@ export default function BeforeTestMenu (){
              </div>
          )
      }
-    else{
+    else if (!get_question_data){
         return (
             <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>
         )
      }
+
+    return (
+            <Container className="mt-4">
+                {checkurl(get_question_data.questionById.videoUrl)?
+                    <Accordion  >
+
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button}  eventKey="1">
+                                Отобразить видео вопрос
+                                {/*{console.log(data?.questionById.videoUrl)}*/}
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                            <ReactPlayer url={get_question_data.questionById.videoUrl} controls autoPlay={true}/>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>: null }
+
+            <TableContainer component={Paper} className="">
+
+                <Table className={classes.table} size='medium' aria-label="a dense table">
+                    {/*<TableHead>*/}
+                    {/*    <TableRow>*/}
+                    {/*        <TableCell> </TableCell>*/}
+                    {/*        <TableCell align="center">Ответы</TableCell>*/}
+                    {/*    </TableRow>*/}
+                    {/*</TableHead>*/}
+                    <TableBody>
+                        {get_question_data.questionById.answers.map((answer: any) => (
+                            <TableRow key={answer.id} hover  role="checkbox"
+                                      onClick = {(e) => {
+                                          changeForRefresh(!forRefresh)
+                                          selectDeselectRow(answer.id)
+                                      }}
+                            selected={selected.indexOf(answer.id) !== -1}>
+                                <TableCell padding="checkbox">
+                                    <Checkbox
+                                        checked={selected.indexOf(answer.id) !== -1}
+                                        inputProps={{ 'aria-labelledby': answer.id }}
+                                    />
+                                </TableCell>
+                                <TableCell align="left">{answer.text}</TableCell>
+                            </TableRow>
+                        ))}
+
+                    </TableBody>
+                </Table>
+            </TableContainer>
+                <Button variant="contained" color="primary" onClick={() =>{changeTestHadBeenStarted(false)}}>
+                    Назад
+                </Button>
+            </Container>
+        )
+
 }

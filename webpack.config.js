1491f21635365330c1f0bbbdb85edfad8f352839
Heightline <teamheightline@mail.ru>
2021-05-13 10:12:07 +0300

    try to fix

--- webpack.config.js
+++ webpack.config.js
@@ -134,12 +134,15 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+
     entry: {
         main: './src/index.js'
     },
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        libraryExport: 'default'
     },
     module: {
         rules: [{

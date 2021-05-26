8531c43c37c7caf50ab579c0596cd983a672b1d1
heightline <teamheightline@mail.ru>
2021-05-26 20:35:44 +0300

    Major algorithm update, move from local storage user data to graph Ql me query data

--- src/Components/Navbar/Navibar.tsx
+++ src/Components/Navbar/Navibar.tsx
@@ -1,29 +1,41 @@
-import * as React from 'react'
+import React from 'react'
 import 'react-bootstrap';
-import  {Navbar, Form, Nav, Dropdown, Button, DropdownButton, Row} from 'react-bootstrap';
-// import IsLogin from '../login';
+import {Navbar, Form, Nav, Dropdown, Button, DropdownButton, Row, Spinner} from 'react-bootstrap';
 import s from'./navibar.module.css';
 import NavSearch from "./search"
-import {gql, useMutation} from "@apollo/client";
-import {useEffect} from "react";
+import {gql, useQuery} from "@apollo/client";
 import { Link } from 'react-router-dom';
 
-const VERIFY_LOGIN = gql`
-    mutation VERIFY_LOGIN($token: String!){
-      verifyToken(token: $token){
-        payload
-        success
-        errors
-      }
+const GET_USER_DATA = gql`
+    query{
+        me{
+            id
+            firstName
+            username
+        }
     }
 `
 
-
+// Как это устроено - в локальном хранилище лежит токен, он подставляется в каждый запрос автоматически,
+// при логине мы получаем этот токен, при анлогине - удаляем, навбар просто пулит каждые 4 секунды значение с этим
+// токеном, если в вернувшемся обьекте есть данные о пользователи - значит токен верен - значит мы вошли
 
 export default function Navibar(){
-
+    const {data: user_data} = useQuery(GET_USER_DATA, {
+        pollInterval: 4000,
+        onCompleted: data => {
+            console.log(data)
+        },
+        onError: error => {
+            console.log(error)
+        }
+    })
+    if (!user_data){
+        return (
+            <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>
+        )
+    }
     return(
-
             <Navbar bg="light" expand="lg" className="col-12 mr-1" >
                 <Navbar.Brand>IOT</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -41,19 +53,16 @@ export default function Navibar(){
                     </Nav>
 
                     <Form  className="col-lg-6">
-                        {/*<FormControl type="text" placeholder="Поиск карточки" className="ml-auto"  />*/}
                         <NavSearch/>
                     </Form>
                     <Button variant="outline-success" className=" ml-3 ">Поиск</Button>
-                    {/*<IsLogin className="ml-auto"/>*/}
                     <Row className="ml-3">
-                    {/*<Navbar.Text className="">User name</Navbar.Text>*/}
-                    {localStorage.getItem("is_login") ==='true'? <Navbar.Text>{localStorage.getItem("user_name")}</Navbar.Text> :
+                    {user_data.me ? <Navbar.Text>{user_data.me.username}</Navbar.Text> :
                         <Nav.Link>
                             <Link className={s.link} to="/login">Войти</Link>
                         </Nav.Link>}
 
-                    {localStorage.getItem("is_login") ==='true'? <div>
+                    {user_data.me? <div>
                             <DropdownButton id="dropdown-navibar-button"  title="" className="ml-4">
                                 <Dropdown.Item>
                                     <Link className={s.link} to="/stat">Статистика </Link>
@@ -65,8 +74,8 @@ export default function Navibar(){
                                     <Link className={s.link} to="/unlogin">Выйти</Link>
                                 </Dropdown.Item>
                         </DropdownButton></div> : null}
-                        {localStorage.getItem("is_login") ==='true'?
-                            null: <Nav.Link className={s.link} href="/registration">Зарегистрироваться</Nav.Link>}
+                        {user_data.me?
+                            null: <Nav.Link><Link className={s.link} to="/registration">Зарегистрироваться</Link></Nav.Link>}
                     </Row>
                 </Navbar.Collapse>
             </Navbar>

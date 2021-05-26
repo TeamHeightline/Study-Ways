99a99c22c5b45557301aaa8acd2595d786d70fd5
heightline <teamheightline@mail.ru>
2021-05-26 20:33:58 +0300

    un login no more trigger global re render of page

--- src/Components/Login/UnLogin.tsx
+++ src/Components/Login/UnLogin.tsx
@@ -9,9 +9,6 @@ export default function UnLogin(){
     localStorage.setItem('is_login', 'false')
     localStorage.setItem('user_name', '')
     localStorage.setItem('refreshToken', 'wrong refresh token')
-    setTimeout(()=>{
-        window.location.reload(false);
-    }, 1000)
     setTimeout(history.push, 500, '/')
     return(
         <div className="col-1 offset-6 justify-content-center mt-4">

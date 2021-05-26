780f5f592a2215082473ec3f162a3988100e2f91
heightline <teamheightline@mail.ru>
2021-05-26 19:05:13 +0300

    fex global reloading after push button

--- src/Components/Menu/MainMenu.tsx
+++ src/Components/Menu/MainMenu.tsx
@@ -1,34 +1,13 @@
 import React from "react";
 import {Card, Container, Row, Button} from "react-bootstrap"
-import logo from "../../img/createUserTestImg.png"
+import { useHistory } from "react-router-dom";
 import logo2 from "../../img/TestingImg.png"
 import logo3 from "../../img/ModernTestEditor.png"
-import { gql, useQuery } from '@apollo/client';
 
-// const QUESTION_DATA = gql`
-//       query {
-//         questionById(id: 5){
-//           text
-//           videoUrl
-//         }}
-//     `
-
-export class MainExperimental extends React.Component{
-    render() {
-        // const { loading, error, data } = useQuery(QUESTION_DATA);
-        // //
-        // // if (loading) console.log('.....loading....')
-        // // if (error) console.log(`Error! ${error.message}`)
-        // console.log(data)
-
+export function MainExperimental (){
+        const history = useHistory();
         return(
             <>
-                {/*<div style={{backgroundImage: `url(${backgroundImg})`,*/}
-                {/*    // height: '100vh',*/}
-                {/*    opacity: 0.1,*/}
-                {/*    backgroundPosition: 'center',*/}
-                {/*    backgroundSize: 'cover',*/}
-                {/*    backgroundRepeat: 'no-repeat'}}>*/}
                 <div className="display-4 text-center ">Создание и редактирование вопросов</div>
                 <Container className="mt-3">
                     <Row>
@@ -39,7 +18,11 @@ export class MainExperimental extends React.Component{
                                 <Card.Text>
                                     После нажатия вы будете перенаправлены в среду для прохождения вопроса
                                 </Card.Text>
-                                <Button variant="primary" href="/test" block size="lg" className="mt-5">Пройти вопрос</Button>
+                                <Button variant="primary" block size="lg" className="mt-5" onClick={() =>{
+                                    history.push("/test");
+                                }}>
+                                    Пройти вопрос
+                                </Button>
                             </Card.Body>
                         </Card>
                         <Card style={{ width: '18rem' }} className="ml-2">
@@ -49,7 +32,12 @@ export class MainExperimental extends React.Component{
                                 <Card.Text>
                                     После нажатия вы будете перенаправлены в  редактор вопросов
                                 </Card.Text>
-                                <Button variant="primary" href="/testeditor"   >Создать/Редактировать вопросы</Button>
+                                <Button variant="primary"
+                                        // href="/testeditor"
+                                        onClick={() =>{
+                                            history.push("/testeditor");
+                                        }}
+                                >Создать/Редактировать вопросы</Button>
                             </Card.Body>
                         </Card>
                     </Row>
@@ -57,6 +45,4 @@ export class MainExperimental extends React.Component{
                 {/*</div>*/}
             </>
         )
-    }
-
 }

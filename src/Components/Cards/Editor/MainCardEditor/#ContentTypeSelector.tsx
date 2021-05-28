ae707c3b818045d435f80f5bba9de22d768f2e37
timofey <teamheightline@mail.ru>
2021-05-28 17:00:22 +0300

    create selector by content type

---
+++ src/Components/Cards/Editor/MainCardEditor/#ContentTypeSelector.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react'
+import Typography from "@material-ui/core/Typography";
+import {Col, Form, Row} from "react-bootstrap";
+import _ from 'lodash'
+
+export default function ContentTypeSelector({cards_data, ...props}: any){
+    const [selectedContentType, setSelectedContentType] = useState<any>(1000000)
+    // props.onChangeSelectedData(_.filter(cards_data, {'cardContentType': "A_" + selectedContentType}))
+    useEffect(() =>{
+        if (Number(selectedContentType) != 1000000) {
+            props.onChangeSelectedData(_.filter(cards_data, {'cardContentType': "A_" + selectedContentType}))
+        } else {
+            props.onChangeSelectedData(cards_data)
+        }}, [cards_data])
+    return(
+        <Row {...props}>
+            <Col className="col-3 mt-2">
+                <Typography variant="h6" gutterBottom className="pr-5">
+                    Тип:
+                </Typography>
+            </Col>
+            <Col className="col-7">
+                <Form.Control
+                    className="mt-1"
+                    // size="lg"
+                    as="select"
+                    value={selectedContentType}
+                    onChange={ (event) => {
+                        // console.log(event)
+                        setSelectedContentType(event.target.value)
+                        if (Number(event.target.value) != 1000000) {
+                            props.onChangeSelectedData(_.filter(cards_data, {'cardContentType': "A_" + event.target.value}))
+                        } else {
+                            props.onChangeSelectedData(cards_data)
+                        }
+                    }}>
+                    <option value={1000000}>Не выбран</option>
+                    <option value={0}>YouTube</option>
+                    <option value={1}>Внешний ресурс</option>
+                    <option value={2}>Изображение</option>
+                </Form.Control>
+            </Col>
+        </Row>
+    )
+}

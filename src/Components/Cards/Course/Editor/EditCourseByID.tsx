20fe15fb5d8138b457822fa3d509cc3f430d25d3
timofey <teamheightline@mail.ru>
2021-06-04 14:03:36 +0300

    comment course structure

--- src/Components/Cards/Course/Editor/EditCourseByID.tsx
+++ src/Components/Cards/Course/Editor/EditCourseByID.tsx
@@ -1,126 +1,26 @@
 import React from 'react'
 import {Col, Row} from "antd";
-import EditCourseItem from "./EditCourseItem";
+import EditCourseItem from "./##EditCourseItem";
 import MainCardEditor from "../../Editor/MainCardEditor/MainCardEditor";
+import CourseFragment from "./#CourseFragment";
+import CourseRow from "./#CourseRow";
 
 export default function EditCourseByID(){
     return(
         <div className="mt-4 ml-4">
             <div style={{overflowY: "scroll"}} className="ml-5 mr-5">
-                <div style={{width: 4000}}>
-                    <br/>
-                    <Row>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                    </Row>
-                    <br/>
-                </div>
-                <div style={{width: 4000}}>
-                    <br/>
-                    <Row>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                    </Row>
-                    <br/>
-                </div>
-                <div style={{width: 4000}}>
-                    <br/>
-                    <Row>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                    </Row>
-                    <br/>
-                </div>
-                <div style={{width: 4000}}>
-                    <br/>
-                    <Row>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div style={{width: 2000}}>
-                                <Row gutter={[16, 16]}>
-                                    {[1,2,3,4,5,6,7,8,9,10,11,12].map((id) =>{
-                                        return(<Col span={2} key={id + "KEY"} >
-                                            <EditCourseItem item_id={id}/>
-                                        </Col>)
-                                    })}
-                                </Row>
-                            </div>
-                        </Col>
-                    </Row>
-                    <br/>
-                </div>
+                <CourseRow/>
+                <CourseRow/>
+                <CourseRow/>
+                <CourseRow/>
             </div>
                 <MainCardEditor/>
         </div>
     )
 }
+// Курс
+//      -строка курса
+//                     -фрагмент строки (12 штук)
+//                                     -редактировать элемент курса
+//
+//

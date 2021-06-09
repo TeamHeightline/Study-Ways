922fe53d2ecbd5773c37c5c080ba101abbaa3e70
timofey <teamheightline@mail.ru>
2021-06-09 22:23:31 +0300

    visualize course based on server data

--- src/Components/Cards/Course/Editor/MainCourseEditor.tsx
+++ src/Components/Cards/Course/Editor/MainCourseEditor.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Button, Card, Container, Paper, Typography} from "@material-ui/core";
 import {gql} from "graphql.macro";
-import {useMutation} from "@apollo/client";
+import {useMutation, useQuery} from "@apollo/client";
 import {CourseLines} from "./EditCourseByID";
 import {Col, Tabs} from "antd";
 import {Row} from "antd";
@@ -19,6 +19,13 @@ const CREATE_COURSE_WITH_DEFAULT_VALUE = gql`
     }
 `
 
+const GET_OWN_COURSE = gql`
+    query GET_OWN_COURSE{
+        cardCourse{
+            courseData
+            id
+        }
+    }`
 export default function MainCourseEditor({...props}: any) {
     const [create_course, {data: create_course_data, error: create_course_error}] = useMutation(CREATE_COURSE_WITH_DEFAULT_VALUE,
         {
@@ -26,14 +33,23 @@ export default function MainCourseEditor({...props}: any) {
                 default_data: CourseLines
             }
         })
+    const {data: own_course_data, refetch} = useQuery(GET_OWN_COURSE)
+    console.log(own_course_data)
     return(
         <div>
             <Container>
                 <Button variant="outlined" color="primary" className="col-12 mt-3 justify-content-center"
-                        size="large"  onClick={() => create_course()}>
+                        size="large"  onClick={() => {
+                    create_course().then(() => refetch())
+                }}>
                     Создать новый курс
                 </Button>
-                <CourseMicroView/>
+
+                {own_course_data?.cardCourse.map((course, cIndex) =>{
+                    return(
+                        <CourseMicroView key={cIndex}/>
+                    )
+                }) }
             </Container>
         </div>
     )

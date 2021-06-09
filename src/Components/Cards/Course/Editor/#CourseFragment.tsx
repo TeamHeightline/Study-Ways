a7530228e7e0c53bd44f3145338077d298f33bf8
timofey <teamheightline@mail.ru>
2021-06-09 22:23:38 +0300

    visualize course based on server data

--- src/Components/Cards/Course/Editor/#CourseFragment.tsx
+++ src/Components/Cards/Course/Editor/#CourseFragment.tsx
@@ -4,23 +4,23 @@ import EditCourseItem from "./##EditCourseItem";
 
 export default function CourseFragment({fragment, ...props}: any){
 
-    console.log(fragment)
-
+    // console.log(fragment)
+    const [Fragment, setFragment] = useState(fragment.CourseFragment)
     return(
         <Col>
             <div style={{width: 2000}}>
                 <Row gutter={[16, 16]}>
-                    {fragment.CourseFragment.map((item, iIndex) =>{
+                    {Fragment.map((item, iIndex) =>{
                         // console.log(item)
                         return(
                             <Col span={2} key={`${iIndex}_${item}`} >
                             <EditCourseItem item_id={item.CourseElement.id}
                                             item_position={iIndex}
                                             updateItem={(new_data) =>{
-                                                // console.log(new_data)
-                                                fragment.CourseFragment[new_data.itemPosition].CourseElement.id = new_data.itemID
-                                                // console.log(fragment.CourseFragment)
-                                                props.updateFragment(fragment)
+                                                const newCourseFragment = Fragment.slice()
+                                                newCourseFragment[iIndex] = new_data
+                                                setFragment(newCourseFragment)
+                                                props.updateFragment(newCourseFragment)
                                             }}
                             />
                         </Col>)

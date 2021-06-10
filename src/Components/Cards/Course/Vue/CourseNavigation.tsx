b9d5aa5fdc3cabceeb6f3a05358d26b3dd648694
timofey <teamheightline@mail.ru>
2021-06-10 16:01:59 +0300

    micro course view based on server data

--- src/Components/Cards/Course/Vue/CourseNavigation.tsx
+++ src/Components/Cards/Course/Vue/CourseNavigation.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import {Row} from "antd";
 import {Button} from "@material-ui/core";
 import NavigationRow from "./#NavigationRow";
-export default function CourseNavigation({...props}: any){
+export default function CourseNavigation({course, ...props}: any){
+    // console.log(course)
     return(
         <div style={{width: 820, overflowY: "scroll", marginBottom:5}}>
-            <NavigationRow/>
-            <NavigationRow/>
-            <NavigationRow/>
-            <NavigationRow/>
+            {course.courseData.map((courseRow, CRI) =>{
+                return(
+                    <NavigationRow key={CRI} courseRow={courseRow}/>
+                )
+            })}
+            {/*<NavigationRow/>*/}
+            {/*<NavigationRow/>*/}
+            {/*<NavigationRow/>*/}
         </div>
     )
 }

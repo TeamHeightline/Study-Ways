b87dfa5440fc4c86a54dec8100d99a0b634bb63c
timofey <teamheightline@mail.ru>
2021-06-10 16:01:47 +0300

    micro course view based on server data

--- src/Components/Cards/Course/Vue/#NavigationRow.tsx
+++ src/Components/Cards/Course/Vue/#NavigationRow.tsx
@@ -3,11 +3,16 @@ import {Button} from "@material-ui/core";
 import {Row} from "antd";
 import RowFragment from "./##RowFragment";
 
-export default function NavigationRow(){
+export default function NavigationRow({courseRow, ...props}: any){
+    // console.log(courseRow)
     return(
-        <Row style={{width: 830*2}}>
-            <RowFragment/>
-            <RowFragment/>
+        <Row style={{width: 830 * courseRow.SameLine.length}}>
+            {courseRow.SameLine.map((rowFragment, rIndex) =>{
+                return(
+                    <RowFragment key={rIndex} rowFragment={rowFragment}/>
+                )
+            })}
+            {/*<RowFragment/>*/}
         </Row>
     )
 }

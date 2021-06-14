253bae820abe56c0ca419e9e35f83d8c1595311f
timofey <teamheightline@mail.ru>
2021-06-14 18:17:09 +0300

    rename one link

--- src/Components/Navbar/Navibar.tsx
+++ src/Components/Navbar/Navibar.tsx
@@ -49,7 +49,7 @@ export default function Navibar(){
                     <Nav className="col-5">
                         <Link className={s.link} to="/">Главная </Link>
                         <Link className={s.link} to="/cards">Катрочки</Link>
-                        <Link className={s.link} to="/test">Пройти тест</Link>
+                        <Link className={s.link} to="/test">Тесты</Link>
                         <Typography className={s.link}> | </Typography>
                         <Link className={s.link} to="/testeditor">Редактировать тест</Link>
                     </Nav>

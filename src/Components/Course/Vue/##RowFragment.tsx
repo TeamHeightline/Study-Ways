f450f141be5d11c9529a75bafbc8f6c8cafb016a
timofey <teamheightline@mail.ru>
2021-06-15 16:36:08 +0300

    fix same key error

--- src/Components/Course/Vue/##RowFragment.tsx
+++ src/Components/Course/Vue/##RowFragment.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {Button} from "@material-ui/core";
 import './RowFragment.css'
 export default function RowFragment({rowFragment, ...props}: any){
-    console.log(rowFragment)
+    // console.log(rowFragment)
     return(
         <>
-            {rowFragment.CourseFragment.map((element) =>{
+            {rowFragment.CourseFragment.map((element, eIndex) =>{
                 return(
                     <Button className="ml-1 mt-1" color={element?.CourseElement?.id ? "primary" : "primary"}
                             variant={element?.CourseElement?.id ? "outlined": "text"}
-                            key={element+ "CourseFragment"}>
+                            key={eIndex + "CourseFragment" + props.rIndex + "RowFragment" + props.CRI + "NavigationRow"}>
                         {element?.CourseElement?.id ? element?.CourseElement?.id : <br/>}
                     </Button>
                 )

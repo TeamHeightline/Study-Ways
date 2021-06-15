c67c2cd38f0e4a081bf012c607eb5203ded7ad5d
timofey <teamheightline@mail.ru>
2021-06-15 17:36:06 +0300

    SHOW COURSE IMAGE IN COURSE MICRO VIEW

--- src/Components/Course/Editor/CourseMicroView.tsx
+++ src/Components/Course/Editor/CourseMicroView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Row} from "antd";
 import {Button, Card, CardActionArea, CardMedia, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/styles";
@@ -20,13 +20,28 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CourseMicroView({course, ...props}: any) {
     const classes = useStyles();
+    const [cardCourseImageURL, setCardCourseImageURL] = useState('');
+    async function getCourseImageData(){
+        fetch("https://iot-experemental.herokuapp.com/cardfiles/course?id=" + course.id)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log('Success:', result);
+                setCardCourseImageURL(result[0].image)
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+    useEffect(() =>{
+        getCourseImageData()
+    }, [course])
     return(
         <div {...props}>
             <Card className={classes.root}>
                 <CardMedia
                     onClick={() => console.log("click")}
                     className={classes.cover}
-                    image="https://content.skyscnr.com/m/5462d448281ea355/original/GettyImages-468945589.jpg?resize=1800px:1800px&quality=100"
+                    image={cardCourseImageURL? cardCourseImageURL: "https://content.skyscnr.com/m/5462d448281ea355/original/GettyImages-468945589.jpg?resize=1800px:1800px&quality=100"}
                 />
                 <CardActionArea style={{ width:260}} onClick={() => {
                     console.log("Click")

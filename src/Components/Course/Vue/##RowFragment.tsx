2701d129c122affa772d83ceb45d2375ab6dde95
timofey <teamheightline@mail.ru>
2021-06-16 18:00:29 +0300

    algorithm for pass all position data of click on button(card)

--- src/Components/Course/Vue/##RowFragment.tsx
+++ src/Components/Course/Vue/##RowFragment.tsx
@@ -9,6 +9,10 @@ export default function RowFragment({rowFragment, ...props}: any){
                 return(
                     <Button className="ml-1 mt-1" color={element?.CourseElement?.id ? "primary" : "primary"}
                             variant={element?.CourseElement?.id ? "outlined": "text"}
+                            onClick={() =>{
+                                props.buttonClick(eIndex)
+                                // console.log(eIndex + "CourseFragment" + props.rIndex + "RowFragment" + props.CRI + "NavigationRow")
+                            }}
                             key={eIndex + "CourseFragment" + props.rIndex + "RowFragment" + props.CRI + "NavigationRow"}>
                         {element?.CourseElement?.id ? element?.CourseElement?.id : <br/>}
                     </Button>

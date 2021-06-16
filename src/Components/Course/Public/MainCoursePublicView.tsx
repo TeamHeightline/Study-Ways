6c5705ed1119d10e77e63ac482ffb48295ce5c9d
timofey <teamheightline@mail.ru>
2021-06-16 18:01:15 +0300

    open card by clicked on button(that provide card ID)

--- src/Components/Course/Public/MainCoursePublicView.tsx
+++ src/Components/Course/Public/MainCoursePublicView.tsx
@@ -3,7 +3,7 @@ import {gql} from "graphql.macro";
 import {useQuery} from "@apollo/client";
 import {Spinner} from "react-bootstrap";
 import CourseMicroView from "../Editor/CourseMicroView";
-
+import Card from '../../Cards/Card'
 const GET_ALL_COURSE = gql`
     query GET_ALL_COURSE{
         cardCourse{
@@ -13,12 +13,33 @@ const GET_ALL_COURSE = gql`
         }
     }`
 export default function MainCoursePublicView({...props}){
-    const [isOpenCourse, setIsOpenCourse] = useState(false)
-    const [openCourseNumber, setOpenCourseNumber] = useState<any>()
+    const [isOpenCard, setIsOpenCard] = useState(false)
+    const [cardPositionData, setCardPositionData] = useState<any>()
     const {data: course_data} = useQuery(GET_ALL_COURSE)
+    const get_card_id_by_position = (cardPositionData) =>{
+        return(course_data.cardCourse[cardPositionData.courseIndex].courseData[cardPositionData.row]
+            .SameLine[cardPositionData.fragment].CourseFragment[cardPositionData.buttonIndex].CourseElement.id)
+    }
     if (!course_data) {
         return (
             <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>
+        )
+    }
+    if(isOpenCard){
+        return (
+            <Card id={get_card_id_by_position(cardPositionData)}
+                  ButtonClick={(data) =>{
+                      if(data === "Next"){
+                          const newCardPositionData = cardPositionData
+                          newCardPositionData.buttonIndex = Number(newCardPositionData.buttonIndex) + 1
+                          setCardPositionData(newCardPositionData)
+                      }
+                  }}
+                  onChange={(data) =>{
+                    if(data ==="goBack"){
+                        setIsOpenCard(false)
+                    }
+            }}/>
         )
     }
     return(
@@ -27,9 +48,26 @@ export default function MainCoursePublicView({...props}){
             {course_data.cardCourse.map((sameCourse, cIndex) =>{
                 return(
                     <CourseMicroView className="ml-5 mt-4" course={sameCourse} key={"Course" + cIndex}
-                    onEdit={(data) =>{
-                        setOpenCourseNumber(data)
-                        setIsOpenCourse(true)
+                                     buttonClick={async (data) =>{
+                                         data.courseIndex=cIndex
+                                         data.courseID=sameCourse.id
+                                         // console.log(data)
+                                         await setCardPositionData(data)
+                                         await setIsOpenCard(true)
+                                         // get_card_id_by_position(data)
+                                     }}
+                    onEdit={(get_data) =>{
+                        const data = {
+                            courseIndex: cIndex,
+                            courseID: get_data,
+                            buttonIndex: 0,
+                            fragment: 0,
+                            row:1
+                        }
+                        console.log(data)
+                        setCardPositionData(data)
+                        setIsOpenCard(true)
+                        get_card_id_by_position(data)
                     }}/>
                 )
             })}

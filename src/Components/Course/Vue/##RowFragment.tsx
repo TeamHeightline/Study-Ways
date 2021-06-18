80c1c4e2b0a9b08987b5edd01ed757c586b6a687
timofey <teamheightline@mail.ru>
2021-06-18 11:34:07 +0300

    [create] Card position visualize in card course navigation!!!

--- src/Components/Course/Vue/##RowFragment.tsx
+++ src/Components/Course/Vue/##RowFragment.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import {Button} from "@material-ui/core";
 import './RowFragment.css'
 export default function RowFragment({rowFragment, ...props}: any){
-    // console.log(rowFragment)
+
     return(
         <>
             {rowFragment.CourseFragment.map((element, eIndex) =>{
+                if(props.cardPositionData && props.cardPositionData.row === props.CRI && props.cardPositionData.fragment === props.rIndex
+                    && eIndex ===props.cardPositionData.buttonIndex){
+                        console.log(true)
+                }
+
                 return(
-                    <Button className="ml-1 mt-1" color={element?.CourseElement?.id ? "primary" : "primary"}
-                            variant={element?.CourseElement?.id ? "outlined": "text"}
+                    <Button className="ml-1 mt-1" color={"primary"}
+                            variant={element?.CourseElement?.id ?
+                                props.cardPositionData && props.cardPositionData.row === props.CRI && props.cardPositionData.fragment === props.rIndex
+                                && eIndex ===props.cardPositionData.buttonIndex ?
+                                    "contained": "outlined": "text"}
                             onClick={() =>{
                                 props.buttonClick(eIndex)
                                 // console.log(eIndex + "CourseFragment" + props.rIndex + "RowFragment" + props.CRI + "NavigationRow")
                             }}
+
                             key={eIndex + "CourseFragment" + props.rIndex + "RowFragment" + props.CRI + "NavigationRow"}>
                         {element?.CourseElement?.id ? element?.CourseElement?.id : <br/>}
                     </Button>

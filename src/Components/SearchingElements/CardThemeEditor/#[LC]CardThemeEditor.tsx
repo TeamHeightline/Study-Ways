1de480971b80c211fafc31360a2a7fdd143bb1ec
timofey <teamheightline@mail.ru>
2021-06-25 20:40:11 +0300

    [add] documentation [create] theme/sub theme/ global theme executor

--- src/Components/SearchingElements/CardThemeEditor/#[LC]CardThemeEditor.tsx
+++ src/Components/SearchingElements/CardThemeEditor/#[LC]CardThemeEditor.tsx
@@ -14,6 +14,9 @@ export default function LCCardThemeEditor(){
     const [selected_value, set_selected_value] = useState()
     const [isCreatingNowCardTheme, setIsCreatingNowCardTheme] = useState(false)
     const [isEditNowCardTheme, setIsEditNowCardTheme] = useState(false)
+    const [all_sub_themes, set_all_sub_themes] = useState<{id: string , name: string}[]>()
+    const [all_themes, set_all_themes] = useState<{id: string, name: string}[]>()
+    const [all_global_themes, set_all_global_themes] = useState<{id: string | undefined, name: string | undefined}[]>()
 
     const {data: my_card_themes_data, } = useQuery<Query, null>(GET_MY_CARD_THEMES)
     const {data: all_card_themes_data, } = useQuery<Query, null>(ALL_CARD_THEMES)
@@ -35,19 +38,37 @@ export default function LCCardThemeEditor(){
         }
     }, [my_card_themes_data]) // подписка на любые изменения в собственных темах
     useEffect(() =>{
-       const __expanded: string[] = [] //собираем адйдишники, чтобы про появление списка он уже был
+        const __expanded: string[] = [] //собираем адйдишники, чтобы про появление списка он уже был
         //раскрыт на этих элементах
+
+        const __all_sub_themes: {id: string , name: string}[] = [] //Нужно, чтобы потом можно было при клике находить эту
+        const __all_themes: {id: string, name: string}[] = [] // подтему/тему/глобальную тему для того, чтобы
+        const __all_global_themes: {id: string | undefined, name: string | undefined}[] = [] //редактировать, ID не
+        // редактируем, на этих данных будут строяться мутации
+
+        //Для того, чтобы элементы списка работали, у них должен быть свой уникатльный ID, проблема в том, что
+        // темы/подтемы/глобальные темы имеют свои независимые айдишники, по этому они могут повторяться, чтобы этого
+        //избежать мы умножаем ID глобальной темы на 10^6, а тем на 10^3, ID подтем не трогаем, таким образом, если
+        // тем или подтем меньше 1000, что все работает шикарно
         if(all_card_themes_data){
             all_card_themes_data?.cardGlobalTheme?.map((sameGlobalTheme) =>{
+                __all_global_themes.push({id: sameGlobalTheme?.id, name: sameGlobalTheme?.name})
                 __expanded.push(String(Number(sameGlobalTheme?.id) * 1000000))
                 sameGlobalTheme?.cardthemeSet.map((sameTheme) =>{
+                    __all_themes.push({id: sameTheme.id, name: sameTheme.name})
                     __expanded.push(String(Number(sameTheme?.id) * 1000))
                     sameTheme?.cardsubthemeSet.map((sameSubTheme) =>{
+                        __all_sub_themes.push({id: sameSubTheme.id, name: sameSubTheme.name})
                         __expanded.push(sameSubTheme.id)
                     })
                 })
             })
+            //После всех расчетов все данные сохраняются в стейт, вся логика работает именно с локальными переменными,
+            //чтобы не было миллионов ререндеров, локальные переменные начинаются с "__"
             setExpanded(__expanded)
+            set_all_global_themes(__all_global_themes)
+            set_all_themes(__all_themes)
+            set_all_sub_themes(__all_sub_themes)
         }
     }, [all_card_themes_data]) //подписка на любые изменения в данных о темах
     return(

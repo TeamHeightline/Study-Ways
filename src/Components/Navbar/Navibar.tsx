612844e2c05fd340b00c43f0137b6d5c45ad1642
timofey <teamheightline@mail.ru>
2021-06-30 14:17:46 +0300

    add mobile navigation

--- src/Components/Navbar/Navibar.tsx
+++ src/Components/Navbar/Navibar.tsx
@@ -7,8 +7,23 @@ import {Navbar, Form, Nav, Dropdown, Button, DropdownButton, Row, Spinner, Col} 
 import s from'./navibar.module.css';
 import NavSearch from "../OLD_NOT_USED/search"
 import {gql, useQuery} from "@apollo/client";
-import { Link } from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {Typography} from "antd";
+import useWindowDimensions from "../../CustomHooks/useWindowDimensions";
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FolderIcon from '@material-ui/icons/Folder';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+import {makeStyles} from "@material-ui/styles";
+import BlurLinearIcon from "@material-ui/icons/BlurLinear";
+import ArtTrackIcon from "@material-ui/icons/ArtTrack";
+import RecentActorsIcon from "@material-ui/icons/RecentActors";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+import EditIcon from '@material-ui/icons/Edit';
 
 const GET_USER_DATA = gql`
     query{
@@ -20,11 +35,33 @@ const GET_USER_DATA = gql`
     }
 `
 
+
 // Как это устроено - в локальном хранилище лежит токен, он подставляется в каждый запрос автоматически,
 // при логине мы получаем этот токен, при анлогине - удаляем, навбар просто пулит каждые 4 секунды значение с этим
 // токеном, если в вернувшемся обьекте есть данные о пользователи - значит токен верен - значит мы вошли
 
 export default function Navibar(){
+    const{height, width} = useWindowDimensions();
+    const [value, setValue] = React.useState('0');//Здесь хронится значение на какой мы странице для
+    // мобильных устройств
+    const history = useHistory();
+
+    const mobileMunuClickHandleChange = (event, newValue) => {
+        console.log(newValue)
+        if(newValue == 0){
+            history.push('/courses')
+        }
+        if(newValue == 1){
+            history.push('/cards')
+        }
+        if(newValue == 2){
+            history.push('/test')
+        }
+        if(newValue == 3){
+            history.push('/editor')
+        }
+        setValue(newValue);
+    };
     const {data: user_data} = useQuery(GET_USER_DATA, {
         // pollInterval: 10000,
         onCompleted: data => {
@@ -37,6 +74,16 @@ export default function Navibar(){
     if (!user_data){
         return (
             <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>
+        )
+    }
+    if(height/width >= 1){
+        return (
+            <BottomNavigation value={value} onChange={mobileMunuClickHandleChange} className="col-12">
+                <BottomNavigationAction label="Курсы" value="0" icon={<BlurLinearIcon />} />
+                <BottomNavigationAction label="Карточки" value="1" icon={<ArtTrackIcon />} />
+                <BottomNavigationAction label="Вопросы" value="2" icon={<DoneAllIcon />} />
+                <BottomNavigationAction label="Редакторы" value="3" icon={<EditIcon />} />
+            </BottomNavigation>
         )
     }
     return(

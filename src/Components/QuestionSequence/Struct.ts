1f6b0ade38bdc6566d877e01daa06adb7b8f6349
timofey <teamheightline@mail.ru>
2021-07-02 18:23:30 +0300

    [change] properties to more friendly settings

--- src/Components/QuestionSequence/Struct.ts
+++ src/Components/QuestionSequence/Struct.ts
@@ -9,11 +9,11 @@ export const question_sequence_struct = {
     settings: {
         can_switch_pages: true,//может ли пользователь сам переключаться между вопросами
         use_random_position_for_questions: true, //Перемешивать ли все вопросы в серии вопросов
-        show_help_text: false,//Показывать подсказки или нет
+        show_help_text: true,//Показывать подсказки или нет
         need_await_full_true_answers: false, //Нужно ли ждать пока пользователь ответит все правильно, или можно переключаться дальше
         use_random_position_for_answers: true, //Перемешивать ли ответы
-        max_sum_of_attempts: 10, //Максимальное количество попыток
-        hard_level: "EASY",// "HARD", "MEDIUM"
+        max_sum_of_attempts: 12, //Максимальное количество попыток
+        hard_level: "MEDIUM",// "HARD", "MEDIUM", "EASY"
         card_themes: []//массив айдишников тех тем, на которые этот вопрос
     },
     sequence: [
@@ -25,7 +25,7 @@ export const question_sequence_struct = {
 
 export const CREATE_QUESTION_SEQUENCE = gql`
     mutation CREATE_QUESTION_SEQUENCE($sequenceData: GenericScalar!){
-        createQuestionSequence(input: {sequenceData: $sequenceData}){
+        createQuestionSequence(input: {sequenceData: $sequenceData,}){
             clientMutationId
         }
     }`

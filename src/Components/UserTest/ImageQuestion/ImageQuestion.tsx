902204e3f05ea8a12aff5d2b04ad333ed54fc3ec
timofey <teamheightline@mail.ru>
2021-07-06 19:51:17 +0300

    refactoring + create tub switch + develop more features

---
+++ src/Components/UserTest/ImageQuestion/ImageQuestion.tsx
@@ -0,0 +1,200 @@
+// https://image-store-iot-experemental.s3.amazonaws.com/question-images/2021/04/11/img020.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA5QESEDVQVQN6BL4P%2F20210411%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20210411T134742Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=25b7888a08c977a1b910e1feced0f9996ee5863d7b20686a4106c02580e4a777
+
+import React, {useEffect, useState} from 'react';
+import {Container, Spinner} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import {useMutation, useQuery} from "@apollo/client";
+import * as _ from "lodash"
+import ImageAnswerNode from "../ImageAnswerNode";
+import axios from "axios";
+import {Alert} from "@material-ui/lab";
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import useWindowDimensions from "../../../CustomHooks/useWindowDimensions";
+import DCMCImageQuestion from "./DCMCImageQuestion";
+import HelpText from "./HelpText";
+import DCPCImageQuestion from "./DCPCImageQuestion";
+import DCAnswers from "./DCAnswers";
+import {GET_QUESTION_DATA, STATISTIC} from "./Struct";
+
+export default function ImageQuestion(props: any) {
+    const questionId = props?.match?.params?.id? props.match.params.id: props.id
+    const [helpLevel, changeHelpLevel] = useState(props?.helpLevel? props.helpLevel: "0");
+    const [answers, setAnswers] = useState<any>([{}])
+    const [kolShowAnswers, setKolShowAnswers] = useState(8)
+    const [questionImgUrl, setQuestionImgUrl] = useState<any>('')
+    const [urlHasBeenPassed, setUrlHasBeenPassed] = useState(false)
+    const [selected, changeSelected] = useState<number[]>([])
+    const [activeWrongAnswerIndex, changeActiveWrongAnswerIndex] = useState(-10)
+    const [errorArray, changeErrorArray] = useState<any[]>([])
+    const [tryingCalculation, changeTryingCalculation] = useState(0)
+    const [oneTimePusshCheckErrorButton, changeOneTimePusshCheckErrorButton] = useState(false)
+    const [showCongratulation, setShowCongratulation] = useState(false)
+    const {width, height} = useWindowDimensions()
+
+    const calculateAfterFetch = async (get_question_data) =>{
+        await setAnswers([])
+        let ans = get_question_data?.questionById?.answers;
+        ans = _.shuffle(ans);
+        const trueAns = _.shuffle(_.filter(ans, {isTrue: true}));
+        const wrongAns = _.filter(ans, {isTrue: false}).slice(0, kolShowAnswers - trueAns.length);
+        const trueAndWrongAnswer = [...trueAns, ...wrongAns]
+        await setAnswers(_.shuffle(trueAndWrongAnswer))
+    }
+    const {data: get_question_data} = useQuery(GET_QUESTION_DATA, {
+            variables: {
+                id: questionId
+            },
+
+        }
+    );
+    useEffect( () => {
+        calculateAfterFetch(get_question_data)
+    }, [get_question_data])
+
+    useEffect( () => {
+        const fetchData = async () => {
+            await setUrlHasBeenPassed(false)
+            await setQuestionImgUrl("")
+            const data = await axios("https://iot-experemental.herokuapp.com/files/question?id=" + questionId)
+            try {
+                await setUrlHasBeenPassed(true)
+                await setQuestionImgUrl(data.data[0].image)
+            }catch (e) {
+                console.log(e)
+            }
+        }
+        fetchData()
+
+    }, [props.id]);
+    const [update_statistic] = useMutation(STATISTIC, {
+        variables: {
+            id: get_question_data?.questionById?.questionstatistic?.id,
+            questionID: questionId,
+            numberOfPasses: get_question_data?.questionById?.questionstatistic?.numberOfPasses ? get_question_data?.questionById?.questionstatistic?.numberOfPasses + 1: 1,
+            sumOfAllAttempts:get_question_data?.questionById?.questionstatistic?.sumOfAllAttempts ? get_question_data?.questionById?.questionstatistic?.sumOfAllAttempts + tryingCalculation: tryingCalculation
+        },
+        onCompleted: data => console.log(data),
+        onError: error => console.log(error)
+    })
+
+
+    async function checkErrors() {
+        if (selected.length !== 0){
+            if(!oneTimePusshCheckErrorButton){
+                changeOneTimePusshCheckErrorButton(true)
+            }
+            changeTryingCalculation(tryingCalculation + 1)
+            await changeErrorArray([])
+            const oErrArr: any[] = []
+            let minCheckQueue = 10000000000000000000000
+            let ActiveWrongAnswerIndexLet = activeWrongAnswerIndex
+            answers.map((question: any, Index: number) => {
+                if ((question.isTrue && (selected.indexOf(question.id) === -1)) || (!question.isTrue && (selected.indexOf(question.id) !== -1))) {
+                    // console.log(question)
+                    if (question.checkQueue < minCheckQueue) {
+                        // changeActiveWrongQuestionId(question.id)
+                        minCheckQueue = question.checkQueue
+                        ActiveWrongAnswerIndexLet = Index
+                    }
+                    oErrArr.push(question.id)
+                }
+
+            })
+            if (oErrArr.length === 0){
+                setShowCongratulation(true)
+                update_statistic()
+            }
+            changeActiveWrongAnswerIndex(ActiveWrongAnswerIndexLet)
+            changeErrorArray(oErrArr)
+        }
+    }
+    const onChangeHelpLevel = (event: any) => changeHelpLevel(event.target.value);
+
+    function selectDeselectAnswer(id: any) {
+        if (selected.indexOf(id) === -1) {
+            const oldSelected = selected
+            oldSelected.push(id)
+            changeSelected(oldSelected)
+        } else {
+            const oldSelected = selected
+            oldSelected.splice(selected.indexOf(id), 1)
+            changeSelected(oldSelected)
+        }
+
+    }
+
+
+    const checkurl = (url: any) => url ? url.replace("http://", "").replace("https://", "").replace("www.", "")
+        .replace("youtu.be/", "youtube.com?v=").replace("youtube.com/watch?v=", "youtube.com?v=").slice(0, 14) === "youtube.com?v=" : false;
+
+    // console.log(get_question_data?.questionById?.questionstatistic?.numberOfPasses)
+    // console.log(get_question_data?.questionById?.questionstatistic?.sumOfAllAttempts)
+    return (
+        <div>
+        {!showCongratulation ?
+        <div className="col-12">
+            <Row>
+                {/*Раздел подсказок текстовых/видео*/}
+                <Row className="justify-content-center">
+                    {/*Отдельный компонент только для телефонов, он располагается до раздела с подсказками*/}
+                    <DCMCImageQuestion height={height} width={width} questionImgUrl={questionImgUrl}
+                                       questionData={get_question_data} onChange={props.onChange} onClick={() => {
+                        props.onChange("goBack")
+                    }} disabled={props.open_from_sequence} value={helpLevel} onChange1={onChangeHelpLevel}
+                                       onClick1={() => checkErrors()}/>
+                    <div className="col-9">
+                        <HelpText errorArray={errorArray} helpLevel={helpLevel} answers={answers}
+                                  activeWrongAnswerIndex={activeWrongAnswerIndex}
+                                  b={checkurl(answers[activeWrongAnswerIndex]?.videoUrl)}/>
+                    </div>
+                    <div className="col-9">
+                        <DCPCImageQuestion height={height} width={width} urlHasBeenPassed={urlHasBeenPassed}
+                                           questionImgUrl={questionImgUrl} questionData={get_question_data} id={props.id}
+                                           onChange={props.onChange} onClick={() => {
+                            props.onChange("goBack")
+                        }} disabled={props.open_from_sequence} value={helpLevel} onChange1={onChangeHelpLevel}
+                                           onClick1={() => checkErrors()}
+                                           canSwitchToPreviousQuestion={props.can_switch_to_previous_question}
+                                           onClick2={() => {
+                                               props.ButtonClick("goToPreviousQuestion")
+                                           }} canSwitchToNextQuestion={props.can_switch_to_next_question} onClick3={() => {
+                            props.ButtonClick("goToNextQuestion")
+                        }}/>
+                    </div>
+                </Row>
+            </Row>
+            {get_question_data && answers ?
+                <DCAnswers height={height} width={width} answers={answers} element={(answer, answerIndex) => {
+                    return (
+                        <div key={answerIndex + "divKey"}>
+                            <ImageAnswerNode
+                                answerIndex={answerIndex}
+                                answer={answer}
+                                selected={selected}
+                                onChange={(e) => {
+                                    selectDeselectAnswer(e)
+                                }}/>
+                            <br/>
+                        </div>
+                    )
+                }}/> : <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>}
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div> :
+            <Container className="mt-5">
+                {!props.open_from_sequence ?
+                <Alert severity="success">
+                    <AlertTitle>Поздравляем</AlertTitle>
+                    Вы успешно прошли тест, колличество попыток - <strong>{tryingCalculation}</strong>
+                </Alert> : <Alert severity="success">
+                        <AlertTitle>Вы успешно прошли вопрос</AlertTitle>
+                        Переходите к следующему
+                    </Alert>}
+            </Container>}
+        </div>
+    )
+}

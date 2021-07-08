428fcabf49d4710c9c6983e55ade32ab4d230a4e
timofey <teamheightline@mail.ru>
2021-07-08 10:49:27 +0300

    mobile hot fix

--- src/Components/QuestionSequence/Public/QSByID.tsx
+++ src/Components/QuestionSequence/Public/QSByID.tsx
@@ -7,11 +7,20 @@ import {Step, StepLabel, Stepper} from "@material-ui/core";
 import ImageQuestion from "../../UserTest/ImageQuestion/ImageQuestion";
 export default function QSByID({props}: any) {
     const [activeQuestionIndex, setQuestionIndex] = useState(0)
+    const [allSequenceError, setSequenceError] = useState<{questionArray: [{oneTrueCheck: false, userTyingArray: [{userErrorsAnswersArray: []}] }]}>({questionArray: [{oneTrueCheck: false, userTyingArray: [{userErrorsAnswersArray: []}] }]})
+    //Массив ответов для всех вопросов, для каждого вопросоа есть переменная, которая отображает, была ли хоть раз вызвана проверка для этого вопроса или нет
+    //и массив попыток для этого вопроса в каждой попытке сеть массив ошибочных ответов, которые дал пользователь
     const {data: q_s_data} = useQuery<Query, any>(QSBYID, {
         variables:{
             id: 19 //вместо передаваемого ID
         }
     })
+    // const checkError= (questionIndex: number, userErrorsAnswersArray: any) =>{
+    //     const __allSequenceError__ : any = {...allSequenceError}
+    //
+    //     __allSequenceError__?.questionArray[questionIndex]?.oneTrueCheck = true
+    //
+    // }
 
     if(!q_s_data){
         return (<Spinner animation="border" variant="success" className=" offset-6 mt-5"/>)
@@ -19,7 +28,7 @@ export default function QSByID({props}: any) {
     console.log(q_s_data?.questionSequenceById?.sequenceData?.settings?.hard_level)
     console.log()
     return(
-        <div>
+        <div className="col-12">
             <Stepper alternativeLabel activeStep={activeQuestionIndex}>
                 {q_s_data?.questionSequenceById?.sequenceData?.sequence?.map((label, lIndex) => (
                     <Step key={lIndex}>

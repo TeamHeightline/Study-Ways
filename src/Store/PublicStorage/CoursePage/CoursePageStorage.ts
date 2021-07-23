dbdda98e245607e3ddb6368259cd21f970cadadc
timofey <teamheightline@mail.ru>
2021-07-23 16:24:29 +0300

    [CoursePageStore.ts] | create | autorun for preload course data | create | positionData observable | create | get_card_id_by_position action | create | disabledNext computed | create | disabledBack computed | create | disabledUp computed | create | disabledDown computed | add    | sort by id for courses

--- src/Store/PublicStorage/CoursePage/CoursePageStorage.ts
+++ src/Store/PublicStorage/CoursePage/CoursePageStorage.ts
@@ -1,26 +1,66 @@
-import {action, makeObservable, observable, reaction} from "mobx";
+import {action, autorun, makeObservable, observable} from "mobx";
 import {ClientStorage} from "../../ApolloStorage/ClientStorage";
 import {GET_ALL_COURSE} from "./Struct";
+import {CardCourseNode, Maybe} from "../../../../SchemaTypes";
+import {sort} from "fast-sort";
 
 class CoursePage{
     constructor() {
         makeObservable(this,{
             clientStorage: observable,
-            get_course_data: action,
             courseArr: observable,
+            positionData: observable,
+            get_course_data: action,
+            get_card_id_by_position: action,
 
         })
-        reaction(()=>this.clientStorage.client, ()=>this.get_course_data())
+        autorun(() => this.get_course_data())
     }
     //Получаем прямой доступ и подписку на изменение в хранилище @client для Apollo (для Query и Mutation)
     clientStorage = ClientStorage
     //Массив всех курсов
-    courseArr = []
+    courseArr:  Maybe<CardCourseNode>[] | any[]  = []
+    //Позиция карточки, которая выбрана в данный момент
+    positionData = {
+        courseIndex: 0,
+        row: 0,
+        fragment: 0,
+        buttonIndex: 0,
+        courseID: 0
+    }
+    //action для получения всех данных о !курсах! и записывание их в courseArr
     get_course_data(){
         this.clientStorage.client.query({query: GET_ALL_COURSE})
             .then((data) => {
-                    console.log(data)
+                    console.log(data?.data?.cardCourse)
+                    this.courseArr = sort(data?.data?.cardCourse).asc((c: any) => c?.id)
             })
     }
+    //парсит courseArr для получения id конкретной карточки в опрделенной позиции
+    get_card_id_by_position(cardPositionData, stepRight = 0, stepUp = 0){
+        // console.log(cardPositionData.row + stepUp)
+        if(cardPositionData.buttonIndex + stepRight >= 0 && cardPositionData.buttonIndex + stepRight <= 11 &&
+            Number(cardPositionData.row) + stepUp >= 0 && Number(cardPositionData.row) + stepUp <= 3){
+            return(CoursePageStorage.courseArr[cardPositionData.courseIndex].courseData[Number(cardPositionData.row) + stepUp]
+                .SameLine[cardPositionData.fragment].CourseFragment[Number(cardPositionData.buttonIndex) + stepRight].CourseElement.id)
+        }else{
+            return null
+        }
+    }
+    //computed для вычислений того, можно ли переклчиться в карточке вперед/назад/вверх/вних
+    get disabledNext(){
+        return(this.get_card_id_by_position(this.positionData, 1) === null)
+    }
+    get disabledBack(){
+        return(this.get_card_id_by_position(this.positionData, -1) === null)
+    }
+    get disabledUp(){
+        return(this.get_card_id_by_position(this.positionData, 0, -1) === null)
+    }
+    get disabledDown(){
+        return(this.get_card_id_by_position(this.positionData, 0, +1) === null)
+    }
+
+
 }
 export const  CoursePageStorage =  new CoursePage()

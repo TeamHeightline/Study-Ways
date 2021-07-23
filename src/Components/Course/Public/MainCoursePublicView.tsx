bfd1b599c6085b7faa08a037102d6476c84f59c8
timofey <teamheightline@mail.ru>
2021-07-23 16:30:19 +0300

    [MainCoursePublicView.tsx] giant changes, migrate from standard logic to mobX Storage

--- src/Components/Course/Public/MainCoursePublicView.tsx
+++ src/Components/Course/Public/MainCoursePublicView.tsx
@@ -3,77 +3,57 @@ import {gql} from "graphql.macro";
 import {useQuery} from "@apollo/client";
 import {Spinner} from "react-bootstrap";
 import CourseMicroView from "../Editor/CourseMicroView";
-import Card from '../../Cards/Card'
-const GET_ALL_COURSE = gql`
-    query GET_ALL_COURSE{
-        cardCourse{
-            id
-            name
-            courseData
-        }
-    }`
-export default function MainCoursePublicView({...props}){
+import {CARD} from '../../Cards/Card'
+import {CoursePageStorage} from "../../../Store/PublicStorage/CoursePage/CoursePageStorage";
+import {observer} from "mobx-react";
+import {autorun, toJS} from "mobx";
+export const MainCoursePublicView = observer(({...props}) => {
     const [isOpenCard, setIsOpenCard] = useState(false)
-    const [cardPositionData, setCardPositionData] = useState<any>()
     const [cardID, setCardID] = useState<any>()
-    const {data: course_data} = useQuery(GET_ALL_COURSE)
-    function get_card_id_by_position(cardPositionData, stepRight = 0, stepUp = 0){
-        // console.log(cardPositionData.row + stepUp)
-        if(cardPositionData.buttonIndex + stepRight >= 0 && cardPositionData.buttonIndex + stepRight <= 11 &&
-            Number(cardPositionData.row) + stepUp >= 0 && Number(cardPositionData.row) + stepUp <= 3){
-            return(course_data.cardCourse[cardPositionData.courseIndex].courseData[Number(cardPositionData.row) + stepUp]
-                .SameLine[cardPositionData.fragment].CourseFragment[Number(cardPositionData.buttonIndex) + stepRight].CourseElement.id)
-        }else{
-            return null
-        }
-    }
-    if (!course_data) {
+    // const {data: course_data} = useQuery(GET_ALL_COURSE)
+    if (!CoursePageStorage.courseArr) {
         return (
             <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>
         )
     }
     if(isOpenCard){
         return (
-            <Card id={cardID}
+            <CARD id={cardID}
                   buttonClick={async data => {
-                      data.courseIndex=cardPositionData.courseIndex
-                      data.courseID=cardPositionData.courseID
-                      if(get_card_id_by_position(data)){
-                          await setCardID(get_card_id_by_position(data))
-                          await setCardPositionData(data)
+                      data.courseIndex=CoursePageStorage.positionData.courseIndex
+                      data.courseID=CoursePageStorage.positionData.courseID
+                      if(CoursePageStorage.get_card_id_by_position(data)){
+                          await setCardID(CoursePageStorage.get_card_id_by_position(data))
+                          CoursePageStorage.positionData = data
                       }
                   }}
                   openFromCourse={true}
-                  cardPositionData={cardPositionData}
-                  disabledNext={get_card_id_by_position(cardPositionData, 1) === null}
-                  disabledBack={get_card_id_by_position(cardPositionData, -1) === null}
-                  disabledUp={get_card_id_by_position(cardPositionData, 0, -1) === null}
-                  disabledDown={get_card_id_by_position(cardPositionData, 0, +1) === null}
-                  course={course_data.cardCourse[cardPositionData.courseIndex]}
+                  cardPositionData={CoursePageStorage.positionData}
+                  course={CoursePageStorage.courseArr[CoursePageStorage.positionData.courseIndex]}
                   ButtonClick={(data) =>{
                       if(data === "Next"){
-                          const newCardPositionData = cardPositionData
+                          const newCardPositionData = CoursePageStorage.positionData
                           newCardPositionData.buttonIndex = Number(newCardPositionData.buttonIndex) + 1
-                          setCardPositionData(newCardPositionData)
-                          setCardID(get_card_id_by_position(newCardPositionData))
+                          CoursePageStorage.positionData = newCardPositionData
+                          setCardID(CoursePageStorage.get_card_id_by_position(newCardPositionData))
                       }
                       if(data === "Back"){
-                          const newCardPositionData = cardPositionData
+                          const newCardPositionData = CoursePageStorage.positionData
                           newCardPositionData.buttonIndex = Number(newCardPositionData.buttonIndex) - 1
-                          setCardPositionData(newCardPositionData)
-                          setCardID(get_card_id_by_position(newCardPositionData))
+                          CoursePageStorage.positionData = newCardPositionData
+                          setCardID(CoursePageStorage.get_card_id_by_position(newCardPositionData))
                       }
                       if(data === "Up"){
-                          const newCardPositionData = cardPositionData
+                          const newCardPositionData = CoursePageStorage.positionData
                           newCardPositionData.row = Number(newCardPositionData.row) - 1
-                          setCardPositionData(newCardPositionData)
-                          setCardID(get_card_id_by_position(newCardPositionData))
+                          CoursePageStorage.positionData = newCardPositionData
+                          setCardID(CoursePageStorage.get_card_id_by_position(newCardPositionData))
                       }
                       if(data === "Down"){
-                          const newCardPositionData = cardPositionData
+                          const newCardPositionData = CoursePageStorage.positionData
                           newCardPositionData.row = Number(newCardPositionData.row) + 1
-                          setCardPositionData(newCardPositionData)
-                          setCardID(get_card_id_by_position(newCardPositionData))
+                          CoursePageStorage.positionData = newCardPositionData
+                          setCardID(CoursePageStorage.get_card_id_by_position(newCardPositionData))
                       }
                   }}
                   onChange={(data) =>{
@@ -86,37 +66,25 @@ export default function MainCoursePublicView({...props}){
     return(
         <div{...props} className="col-12" style={{overflowY: "scroll"}}>
             {/*Потом убрать reverse*/}
-            {course_data.cardCourse.map((sameCourse, cIndex) =>{
+            {CoursePageStorage.courseArr.map((sameCourse, cIndex) =>{
                 return(
                     <div key={"Course" + cIndex} className="col-12" style={{overflowY: "scroll"}}>
                     <CourseMicroView className="ml-lg-5 mt-4" course={sameCourse}
                                      buttonClick={async (data) =>{
                                          data.courseIndex=cIndex
-                                         data.courseID=sameCourse.id
-                                         // console.log(data)
-                                         if(get_card_id_by_position(data)){
-                                             await setCardID(get_card_id_by_position(data))
-                                             await setCardPositionData(data)
+                                         data.courseID=sameCourse?.id
+                                         if(CoursePageStorage.get_card_id_by_position(data)){
+                                             await setCardID(CoursePageStorage.get_card_id_by_position(data))
+                                             CoursePageStorage.positionData = data
                                              await setIsOpenCard(true)
                                          }
-                                         // get_card_id_by_position(data)
                                      }}
-                    onEdit={(get_data) =>{
-                        // const data = {
-                        //     courseIndex: cIndex,
-                        //     courseID: get_data,
-                        //     buttonIndex: 0,
-                        //     fragment: 0,
-                        //     row:1
-                        // }
+                    onEdit={() =>{
                         console.log("_")
-                        // setCardPositionData(data)
-                        // setIsOpenCard(true)
-                        // get_card_id_by_position(data)
                     }}/>
                     </div>
                 )
             })}
         </div>
     )
-}
+})

c1f56e0bc9a64afaf7cbd7ea083ca9081ead838b
timofey <teamheightline@mail.ru>
2021-07-23 13:00:34 +0300

    remove non used import in CourseMicroView.tsx

--- src/Components/Course/Editor/CourseMicroView.tsx
+++ src/Components/Course/Editor/CourseMicroView.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Row} from "antd";
-import {Button, Card, CardActionArea, CardMedia, Typography} from "@material-ui/core";
+import {Card, CardActionArea, CardMedia, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/styles";
 import CourseNavigation from "../Vue/CourseNavigation";
 import Divider from '@material-ui/core/Divider';

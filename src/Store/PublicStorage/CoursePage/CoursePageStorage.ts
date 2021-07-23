997be105be2ef02b9ab40826ee511d6e60234bf7
timofey <teamheightline@mail.ru>
2021-07-23 17:59:54 +0300

    [CoursePageStore.ts] | add | inCardButtonClickedHandler (action) | fix | logic error with use observable object without toJS

--- src/Store/PublicStorage/CoursePage/CoursePageStorage.ts
+++ src/Store/PublicStorage/CoursePage/CoursePageStorage.ts
@@ -1,4 +1,4 @@
-import {action, autorun, makeObservable, observable} from "mobx";
+import {action, autorun, computed, makeObservable, observable, toJS} from "mobx";
 import {ClientStorage} from "../../ApolloStorage/ClientStorage";
 import {GET_ALL_COURSE} from "./Struct";
 import {CardCourseNode, Maybe} from "../../../../SchemaTypes";
@@ -7,19 +7,26 @@ import {sort} from "fast-sort";
 class CoursePage{
     constructor() {
         makeObservable(this,{
-            clientStorage: observable,
+            clientStorage: observable ,
             courseArr: observable,
             positionData: observable,
+            selectedCardID: observable,
             get_course_data: action,
             get_card_id_by_position: action,
-
+            inCardButtonClickedHandler: action,
+            disabledNext: computed,
+            disabledBack: computed,
+            disabledUp: computed,
+            disabledDown: computed
         })
         autorun(() => this.get_course_data())
     }
     //Получаем прямой доступ и подписку на изменение в хранилище @client для Apollo (для Query и Mutation)
     clientStorage = ClientStorage
+
     //Массив всех курсов
     courseArr:  Maybe<CardCourseNode>[] | any[]  = []
+
     //Позиция карточки, которая выбрана в данный момент
     positionData = {
         courseIndex: 0,
@@ -28,12 +35,16 @@ class CoursePage{
         buttonIndex: 0,
         courseID: 0
     }
+
+    //ID выбранной карточки
+    selectedCardID = 0
+
     //action для получения всех данных о !курсах! и записывание их в courseArr
     get_course_data(){
         this.clientStorage.client.query({query: GET_ALL_COURSE})
             .then((data) => {
-                    console.log(data?.data?.cardCourse)
-                    this.courseArr = sort(data?.data?.cardCourse).asc((c: any) => c?.id)
+                    //используем сортировку для того, чтобы поставить все курсы в порядке ID
+                    this.courseArr = sort(data?.data?.cardCourse).desc((c: any) => c?.id)
             })
     }
     //парсит courseArr для получения id конкретной карточки в опрделенной позиции
@@ -47,6 +58,36 @@ class CoursePage{
             return null
         }
     }
+
+    //Обработчик реакций на нажатия кнопок навигации в карточке
+    inCardButtonClickedHandler(clickedButtonName){
+        if(clickedButtonName === "Next"){
+            //!!!Очень важно!!! this.positionData - ОБЪЕКТ, его нужно преобразовывать через toJS
+            const newCardPositionData = toJS(this.positionData)
+            newCardPositionData.buttonIndex = Number(newCardPositionData.buttonIndex) + 1
+            this.positionData = newCardPositionData
+            this.selectedCardID = this.get_card_id_by_position(newCardPositionData)
+        }
+        if(clickedButtonName === "Back"){
+            const newCardPositionData = toJS(this.positionData)
+            newCardPositionData.buttonIndex = Number(newCardPositionData.buttonIndex) - 1
+            this.positionData = newCardPositionData
+            this.selectedCardID = this.get_card_id_by_position(newCardPositionData)
+        }
+        if(clickedButtonName === "Up"){
+            const newCardPositionData = toJS(this.positionData)
+            newCardPositionData.row = Number(newCardPositionData.row) - 1
+            this.positionData = newCardPositionData
+            this.selectedCardID = this.get_card_id_by_position(newCardPositionData)
+        }
+        if(clickedButtonName === "Down"){
+            const newCardPositionData = toJS(this.positionData)
+            newCardPositionData.row = Number(newCardPositionData.row) + 1
+            this.positionData = newCardPositionData
+            this.selectedCardID = this.get_card_id_by_position(newCardPositionData)
+        }
+    }
+
     //computed для вычислений того, можно ли переклчиться в карточке вперед/назад/вверх/вних
     get disabledNext(){
         return(this.get_card_id_by_position(this.positionData, 1) === null)

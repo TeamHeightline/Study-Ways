af87dfac2491b4bd026bc8ca6ed0f89771497a95
timofey <teamheightline@mail.ru>
2021-07-31 16:33:55 +0300

    scroll dose not wwork in questions...

--- src/Components/UserTest/ImageQuestion/DCAnswers.tsx
+++ src/Components/UserTest/ImageQuestion/DCAnswers.tsx
@@ -1,8 +1,9 @@
 import Row from "react-bootstrap/Row";
 import React from "react";
+import './Style.css'
 
 export default function DCAnswers(props: { height: number, width: number, answers: any, element: (answer, answerIndex) => JSX.Element }) {
-    return <div style={{overflowY: "scroll"}}>
+    return <div style={{overflowY: "scroll", msOverflowStyle: "scrollbar",}} className="enableScrollbar">
         <div
             style={{width: props.height / props.width >= 1 ? props.answers.length * (props.width + 60) : props.answers.length * 400}}>
             <Row className="justify-content-around">

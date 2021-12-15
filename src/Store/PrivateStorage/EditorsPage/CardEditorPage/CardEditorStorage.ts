b604c3fbff9d4ad3e572b6b21fd7aeac92cf6ae2
timofey <teamheightline@mail.ru>
2021-12-15 13:27:14 +0300

    Editor Algorithm - add function for manual send value

--- src/Store/PrivateStorage/EditorsPage/CardEditorPage/CardEditorStorage.ts
+++ src/Store/PrivateStorage/EditorsPage/CardEditorPage/CardEditorStorage.ts
@@ -1,8 +1,8 @@
 import {makeAutoObservable} from "mobx";
 import {ClientStorage} from "../../../ApolloStorage/ClientStorage";
 import {UserStorage} from "../../../UserStore/UserStore";
-import {GET_CARD_DATA_BY_ID} from "./Struct";
-import {CardNode} from "../../../../SchemaTypes";
+import {GET_CARD_DATA_BY_ID, GET_MY_CARD_AUTHOR} from "./Struct";
+import {CardAuthorNode, CardNode} from "../../../../SchemaTypes";
 import { computedFn } from "mobx-utils"
 export type card_object_fields = keyof CardNode
 
@@ -30,6 +30,17 @@ class CardEditorStorage{
             }
         }
     }
+    // ---------------------раздел работы с авторами карточек---------------------------------------
+    all_my_card_authors: CardAuthorNode[] | undefined = undefined
+
+    loadCardAuthorsFromServer(){
+        if(this.userStorage.userAccessLevel === "TEACHER" || this.userStorage.userAccessLevel === "ADMIN") {
+            this.clientStorage.client.query({query: GET_MY_CARD_AUTHOR, fetchPolicy: "network-only"})
+                .then((response) => (response.data.me.cardauthorSet))
+                .then((authors_data) => this.all_my_card_authors = authors_data)
+        }
+    }
+
     //----------------------раздел работы с данными в самом редакторе ------------------------------
 
     card_object: CardNode | undefined = undefined
@@ -47,6 +58,13 @@ class CardEditorStorage{
                    card_object = this.card_object) => ({target}) =>{
         if(card_object && card_object[field]){
             card_object[field] = target[eventField]
+        }
+    }
+
+    changeFieldByValue(field: card_object_fields | number, value: string | number | boolean,
+                       card_object = this.card_object){
+        if(card_object && card_object[field]){
+            card_object[field] = value
         }
     }
 }

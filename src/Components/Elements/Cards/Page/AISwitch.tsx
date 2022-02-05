889e30da6ae81fa33723b721d731012c92766ab5
timofey <teamheightline@mail.ru>
2022-02-05 17:05:23 +0300

    fix issue with change searchType after goBack in Card

--- src/Components/Elements/Cards/Page/AISwitch.tsx
+++ src/Components/Elements/Cards/Page/AISwitch.tsx
@@ -2,7 +2,9 @@ import {observer} from "mobx-react";
 import React from 'react';
 import Switch from '@mui/material/Switch';
 import {Stack, Typography} from "@mui/material";
-import {useHistory, useRouteMatch} from "react-router-dom";
+import {useHistory, useRouteMatch, Redirect} from "react-router-dom";
+import useQueryParams from "../../../../CustomHooks/useQueryParams";
+
 
 interface IAISwitchProps extends React.HTMLAttributes<HTMLDivElement>{
 
@@ -12,22 +14,31 @@ interface IAISwitchProps extends React.HTMLAttributes<HTMLDivElement>{
 export const AISwitch = observer(({...props}: IAISwitchProps) =>{
     const history = useHistory()
     const { path } = useRouteMatch();
+    const queryParams = useQueryParams();
+
+
     const handleOnChange = (e) =>{
-        if(e.target.checked){
-            history.push(path + "/AISearch")
+        if(!e.target.checked){
+            history.push(path + "?searchType=AISearch")
         } else {
-            history.push(path + "/DSearch")
+            history.push(path + "?searchType=DSearch")
         }
+    }
+    const searchType = queryParams.get("searchType")
+
+    if(!queryParams.get("searchType")){
+        return (<Redirect to={`${path}?searchType=AISearch`}/>)
     }
     return(
         <div {...props}>
             <Stack  direction={"row"}  alignItems={"center"} justifyContent={"center"}>
                 <Typography>
-                    Алгоритмический поиск
-                </Typography>
-                <Switch  defaultChecked color="secondary" onChange={handleOnChange}/>
-                <Typography>
                     Персонализированный поиск
+                </Typography>
+                <Switch  defaultChecked={!(searchType === "AISearch")}
+                         color="secondary" onChange={handleOnChange}/>
+                <Typography>
+                    Алгоритмический поиск
                 </Typography>
             </Stack>
         </div>

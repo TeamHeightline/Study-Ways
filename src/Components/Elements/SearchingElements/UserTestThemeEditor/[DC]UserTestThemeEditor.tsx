8fc712a8e29312509423a0222890550f5abe9942
timofey <teamheightline@mail.ru>
2022-03-23 13:18:37 +0300

    Remove bootstrap from User test theme editor

--- src/Components/Elements/SearchingElements/UserTestThemeEditor/[DC]UserTestThemeEditor.tsx
+++ src/Components/Elements/SearchingElements/UserTestThemeEditor/[DC]UserTestThemeEditor.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { DataGrid } from '@mui/x-data-grid';
-import {Row, Spinner} from "react-bootstrap";
-import {Button, Fab} from "@mui/material";
+import {DataGrid} from '@mui/x-data-grid';
+import {Button, CircularProgress, Fab, Paper, Stack} from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AddIcon from "@mui/icons-material/Add";
 import TextField from "@mui/material/TextField";
 
-export default function DCUserTestThemeEditor({...props}: any){
-    if(!props.rowsHasBeenCalculated){
+export default function DCUserTestThemeEditor({...props}: any) {
+    if (!props.rowsHasBeenCalculated) {
         return (
-            <Spinner animation="border" variant="success" className=" offset-6 mt-5"/>
+            <Stack alignItems={"center"}>
+                <CircularProgress/>
+            </Stack>
         )
     }
-    return(
+    return (
         <div>
             <div style={{maxWidth: 600, height: 470}}>
                 <div style={{height: 400}}>
@@ -23,20 +24,19 @@ export default function DCUserTestThemeEditor({...props}: any){
                               }}
                     />
                 </div>
-                <Row className="col-md-3 offset-md-9 col-12 mt-2">
+                <Stack justifyContent={"end"} direction={"row"} sx={{pt: 1}} spacing={1}>
                     <Fab color="primary"
-                         onClick={() =>{
-                             if(props.isCreatingNowUserTestTheme){
+                         onClick={() => {
+                             if (props.isCreatingNowUserTestTheme) {
                                  props.setIsCreatingNowUserTestTheme(false)
                              }
                              props.setIsEditNowUserTestTheme(!props.isEditNowUserTestTheme)
                          }}>
-                        <SettingsIcon />
+                        <SettingsIcon/>
                     </Fab>
                     <Fab color="primary"
-                         className="ml-2"
-                         onClick={() =>{
-                             if(props.isEditNowUserTestTheme){
+                         onClick={() => {
+                             if (props.isEditNowUserTestTheme) {
                                  props.setIsEditNowUserTestTheme(false)
                              }
                              props.setIsCreatingNowUserTestTheme(!props.isCreatingNowUserTestTheme)
@@ -44,47 +44,49 @@ export default function DCUserTestThemeEditor({...props}: any){
                         <AddIcon/>
 
                     </Fab>
-                </Row>
-                {props.isEditNowUserTestTheme && <div>
-                    <TextField
-                        className="ml-2"
-                        id="standard-multiline-flexible"
-                        label="Имя автора"
-                        fullWidth
-                        value={props.activeEditUserTestThemeName}
-                        onChange={(e) =>{
-                            props.setActiveEditUserTestThemeName(e.target.value)}
-                        }
-                    />
-                    <Row className="mt-2 ml-2">
-                        <Button variant="contained" color="primary" onClick={() =>{
-                            props.update_theme()}}>
-                            Сохранить
-                        </Button>
-                        {props.update_theme_loading &&
-                        <Spinner animation="border" variant="success" className="ml-2 mt-2"/>}
-                    </Row>
-                </div>}
-                {props.isCreatingNowUserTestTheme && <div>
-                    <TextField
-                        className="ml-2"
-                        id="standard-multiline-flexible"
-                        label="Название новой темы"
-                        fullWidth
-                        value={props.nameOfNewUserTestTheme}
-                        onChange={(e) =>{
-                            props.setNameOfNewUserTestTheme(e.target.value)}
-                        }
-                    />
-                    <Row className="mt-2 ml-2">
-                        <Button variant="contained" color="primary" onClick={() =>{
-                            props.create_theme()}}>
-                            Сохранить
-                        </Button>
-                        {props.create_theme_loading &&
-                        <Spinner animation="border" variant="success" className="ml-2 mt-2"/>}
-                    </Row>
-                </div>}
+                </Stack>
+                {props.isEditNowUserTestTheme &&
+                    <Paper elevation={0} sx={{mt: 1}}>
+                        <TextField
+                            label="Название темы"
+                            fullWidth
+                            value={props.activeEditUserTestThemeName}
+                            onChange={(e) => {
+                                props.setActiveEditUserTestThemeName(e.target.value)
+                            }
+                            }
+                        />
+                        <Stack direction={"row"} sx={{mt: 1}}>
+                            <Button variant="contained" color="primary" onClick={() => {
+                                props.update_theme()
+                            }}>
+                                Сохранить
+                            </Button>
+                            {props.update_theme_loading &&
+                                <CircularProgress/>}
+                        </Stack>
+                    </Paper>}
+                {props.isCreatingNowUserTestTheme &&
+                    <Paper elevation={0} sx={{mt: 1}}>
+                        <TextField
+                            label="Название новой темы"
+                            fullWidth
+                            value={props.nameOfNewUserTestTheme}
+                            onChange={(e) => {
+                                props.setNameOfNewUserTestTheme(e.target.value)
+                            }
+                            }
+                        />
+                        <Stack direction={"row"} sx={{mt: 1}}>
+                            <Button variant="contained" color="primary" onClick={() => {
+                                props.create_theme()
+                            }}>
+                                Сохранить
+                            </Button>
+                            {props.create_theme_loading &&
+                                <CircularProgress/>}
+                        </Stack>
+                    </Paper>}
             </div>
         </div>
     )

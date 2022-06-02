e43c1124511486756ee49d06e4db18267b44ff3d
Timofey Chirtsov <teamheightline@mail.ru>
2022-06-02 15:32:45 +0300

    2 июня - удаление ненужного редактора тем вопросов

--- src/Components/PrivatePages/SearchingElementsEditor.tsx
+++ src/Components/PrivatePages/SearchingElementsEditor.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import LCUserTestThemeEditor from "../Elements/SearchingElements/UserTestThemeEditor/[LC]UserTestThemeEditor";
 import {Grid, Stack, Typography} from "@mui/material";
 import ThemeEditor from "../Elements/ThemeTree/ThemeEditor";
 import {isMobileHook} from "../../CustomHooks/isMobileHook";
@@ -10,23 +9,12 @@ export default function SearchingElementsEditor({...props}: any) {
         <div {...props}>
             <Stack direction={"column"} alignItems={"center"}>
                 <Typography variant={"h3"} sx={{pt: 4, pb: 2}}>
-                    Редактор связанных тем
+                    Редактор тем для вопросов и ресурсов
                 </Typography>
             </Stack>
             <Grid container justifyContent={"center"}
                   style={{paddingLeft: isMobile ? 12 : 48}}>
                 <ThemeEditor/>
-            </Grid>
-            <Stack direction={"column"} alignItems={"center"} sx={{pb: 4}}>
-                <Typography variant={"h3"}>
-                    Темы для вопросов
-                </Typography>
-            </Stack>
-            <Grid container justifyContent={"center"}
-                  style={{paddingLeft: isMobile ? 12 : 48}}>
-                <Grid item xs={12} md={6}>
-                    <LCUserTestThemeEditor/>
-                </Grid>
             </Grid>
         </div>
     )

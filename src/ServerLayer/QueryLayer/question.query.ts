74a6d257dcf6acaa6f540e47f030ef2242751b0a
Timofey Chirtsov <teamheightline@mail.ru>
2022-06-07 00:08:29 +0300

    5 июня - добавлена функция для получения и дешифровки вопроса

--- src/ServerLayer/QueryLayer/question.query.ts
+++ src/ServerLayer/QueryLayer/question.query.ts
@@ -1,12 +1,23 @@
 import axiosClient from "./config";
-import {IQuestionPreviewData, QuestionAuthorI} from "../Types/question.type";
+import {IQuestionPreviewData, IQuestionWithAnswers, QuestionAuthorI} from "../Types/question.type";
+import CryptoJS from "crypto-js";
 
-export async function loadQuestionAuthors(): Promise<QuestionAuthorI[]>{
+
+export async function loadQuestionAuthors(): Promise<QuestionAuthorI[]> {
     return axiosClient.get("/question/authors")
         .then((res) => res.data.question_authors)
 }
 
-export async function loadAllQuestions(): Promise<IQuestionPreviewData[]>{
+export async function loadAllQuestions(): Promise<IQuestionPreviewData[]> {
     return axiosClient.get("/question/all/data")
         .then((res) => res.data.questions_data)
+}
+
+export async function loadQuestionByID(id: string): Promise<IQuestionWithAnswers> {
+    return axiosClient.get(`/question/encrypted/${id}`)
+        .then((res) => {
+            const decryptedQuestionData = CryptoJS.AES.decrypt(res.data.encryptedQuestionData, "sw-secret-key")
+                .toString(CryptoJS.enc.Utf8)
+            return JSON.parse(decryptedQuestionData)
+        })
 }

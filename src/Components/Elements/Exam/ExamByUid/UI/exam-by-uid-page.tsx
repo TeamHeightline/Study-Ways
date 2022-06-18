dffaeedd6fc0f4905d0fa9547b6137e97a67ecbb
Timofey Chirtsov <teamheightline@mail.ru>
2022-06-18 16:13:58 +0300

    14 июня - экзамен - исправлена ошибка с тем, что когда человек еще не вошел в профиль, отправляется запрос и ему выдает ошибку не авторизован

--- src/Components/Elements/Exam/ExamByUid/UI/exam-by-uid-page.tsx
+++ src/Components/Elements/Exam/ExamByUid/UI/exam-by-uid-page.tsx
@@ -1,4 +1,4 @@
-import {Paper} from "@mui/material";
+import {CircularProgress, Paper, Stack} from "@mui/material";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {loadExamNameAsync, openExamPageAsync} from "../redux-store/async-actions";
@@ -6,15 +6,26 @@ import UIExamName from "./ui-exam-name";
 import UIExamQuestionProgress from "./mini-question-selector/ui-exam-questions-progress";
 import QuestionPlayer from "./question-player/question-player";
 import {isMobileHook} from "../../../../../CustomHooks/isMobileHook";
+import {UserStorage} from "../../../../../Store/UserStore/UserStore";
 
 
 export default function ExamByUIDPge({...props}) {
     const isMobile = isMobileHook()
     const dispatch: any = useDispatch();
+
     useEffect(() => {
-        dispatch(openExamPageAsync(props.match.params.uid))
-        dispatch(loadExamNameAsync(props.match.params.uid))
-    }, [props.match.params.uid])
+        if (UserStorage.isLogin) {
+            dispatch(openExamPageAsync(props.match.params.uid))
+            dispatch(loadExamNameAsync(props.match.params.uid))
+        }
+    }, [props.match.params.uid, UserStorage.isLogin])
+    if (!UserStorage.isLogin) {
+        return (
+            <Stack alignItems="center">
+                <CircularProgress/>
+            </Stack>
+        )
+    }
     return (
         <Paper elevation={0} {...props} sx={{p: isMobile ? 0 : 2}}>
             <UIExamName/>

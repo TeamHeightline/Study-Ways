87e21e406911484a1de3fa1faf2cd2be2d9e010e
Timofey Chirtsov <teamheightline@mail.ru>
2022-08-21 16:10:15 +0300

    Сделал более понятное отображение похожих карточек, теперь они расположены таблицей и при нажатии, происходит переход на страницу новой карточки,а не как раньше, когда карточка появлялась ниже и все превращалось в какую-то башню

--- src/Components/Elements/Cards/CardByID/UI/card-main-content.tsx
+++ src/Components/Elements/Cards/CardByID/UI/card-main-content.tsx
@@ -19,7 +19,7 @@ const CardMainContent = observer(({card_store, ...props}: ICardMainContentProps)
     const isShowImageContent = isRemoteResourceContentType || isSimpleImageContentType
 
     return (
-        <Paper elevation={0} {...props}>
+        <Paper elevation={0} sx={{height: {xs: 200, md: 440}}} {...props}>
             {isYoutubeContentType &&
                 <YoutubeContent card_store={card_store}/>}
             {isShowImageContent &&
--- src/Components/Elements/Cards/CardByID/UI/similar-cards.tsx
+++ src/Components/Elements/Cards/CardByID/UI/similar-cards.tsx
@@ -1,9 +1,10 @@
 import {observer} from "mobx-react";
 import React from 'react';
 import {PaperProps} from "@mui/material/Paper/Paper";
-import {Paper} from "@mui/material";
+import {Grid, Paper, Stack, Typography} from "@mui/material";
 import {CardByIDStore} from "../Store/CardByIDStore";
-import {MainDirection} from "../../../Direction/UI/MainDirection";
+import CardMicroView from "../../CardView/CardMicroView";
+import {useNavigate} from "react-router-dom";
 
 
 interface ISimilarCardsProps extends PaperProps {
@@ -12,10 +13,27 @@ interface ISimilarCardsProps extends PaperProps {
 }
 
 const SimilarCards = observer(({card_store, ...props}: ISimilarCardsProps) => {
+    const navigate = useNavigate()
+
+    const onCardClick = (cardID) => () => {
+        navigate("/card/" + cardID)
+    }
     return (
         <Paper elevation={0} {...props}>
-            {card_store.dataForDirection &&
-                <MainDirection directionDataProps={card_store.dataForDirection}/>}
+            {/*{card_store.dataForDirection &&*/}
+            {/*    <MainDirection directionDataProps={card_store.dataForDirection}/>}*/}
+            <Stack alignItems={"center"} sx={{mt: 2}}>
+                <Typography variant={"h3"}>
+                    Похожие карточки
+                </Typography>
+            </Stack>
+            <Grid container spacing={2} justifyContent={"space-around"}>
+                {card_store.similarCardsID?.map((cardID) =>
+                    <Grid item key={cardID}>
+                        <CardMicroView cardID={Number(cardID)} onClick={onCardClick(cardID)}/>
+                    </Grid>
+                )}
+            </Grid>
         </Paper>
     )
 })

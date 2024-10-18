02c59dbcb4cab2e3b56ed55619361db3318cb62d
timofei <teamheightline@mail.ru>
2024-10-18 15:21:55 +0300

    Просмотр карточек с внешним ресурсом - сделал затемнение фона изображения и добавил иконку открытия

--- src/Pages/Cards/CardByID/UI/card-image.tsx
+++ src/Pages/Cards/CardByID/UI/card-image.tsx
@@ -1,9 +1,11 @@
 import {observer} from "mobx-react";
 import React from 'react';
 import {PaperProps} from "@mui/material/Paper/Paper";
-import {Card, CardActionArea} from "@mui/material";
+import {Card, CardActionArea, Stack, Typography} from "@mui/material";
 import {CardByIDStore} from "../Store/CardByIDStore";
-
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import FileOpenIcon from '@mui/icons-material/FileOpen';
+import WebIcon from '@mui/icons-material/Web';
 
 interface ICardImageProps extends PaperProps {
     card_store: CardByIDStore
@@ -33,15 +35,31 @@ const CardImage = observer(({card_store, ...props}: ICardImageProps) => {
                   height: {xs: 200, md: 540},
                   // backgroundSize: "cover",
                   backgroundImage: card_image,
+                  position: 'relative',
               }}>
             {isClickableImage &&
                 <CardActionArea
                     sx={{
                         width: "100%",
-                        height: "100%"
+                        height: "100%",
+                        backdropFilter: 'brightness(40%)'
                     }}
                     onClick={goToRemoteResource}
                 />}
+            {isClickableImage &&
+                <div style={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: "translate(-50%, -50%)",
+                    pointerEvents: 'none'
+                }}>
+                    <Stack direction={'row'} spacing={2}
+                           alignItems={'center'}
+                           justifyContent={'center'}>
+                        <FileOpenIcon sx={{fontSize: 80}}/>
+                    </Stack>
+                </div>}
 
         </Card>
     )

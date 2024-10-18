83e4ec000b998949d5c0801306196e4a66d82d57
timofei <teamheightline@mail.ru>
2024-10-18 19:18:14 +0300

    Меню Сделал кнопку входа обычной кнопкой

---
+++ src/SharedComponents/Navbar/LoginButton.tsx
@@ -0,0 +1,17 @@
+import {observer} from "mobx-react";
+import {Button} from "@mui/material";
+import React from "react";
+import {useAuth0} from "@auth0/auth0-react";
+import LoginIcon from '@mui/icons-material/Login';
+
+export const LoginButton = observer(() => {
+    const {loginWithPopup} = useAuth0();
+
+    return (
+        <Button startIcon={<LoginIcon/>} sx={{color: 'white'}} onClick={() => {
+            loginWithPopup()
+        }}>
+            Войти
+        </Button>
+    )
+})
--- src/SharedComponents/Navbar/Menu.tsx
+++ src/SharedComponents/Navbar/Menu.tsx
@@ -1,29 +1,17 @@
-import {IconButton, Stack} from "@mui/material";
+import {Stack} from "@mui/material";
 import {UserStorage} from "../../Store/UserStore/UserStore";
 import PagesMenu from "./PagesMenu";
 import React from "react";
-import {useAuth0} from "@auth0/auth0-react";
 import {observer} from "mobx-react";
 import PersonalMenu from "./PersonalMenu";
-import LoginIcon from '@mui/icons-material/Login';
+import {LoginButton} from "./LoginButton";
 
 const Menu = observer(() => {
-    const {loginWithPopup} = useAuth0();
-
     return (
         <Stack direction={"row"} sx={{backdropFilter: "none"}}>
             <Stack direction={"row"} spacing={2}>
                 <PagesMenu/>
-                {UserStorage.isLogin ?
-                    <>
-                        <PersonalMenu/>
-                    </>
-                    :
-                    <IconButton sx={{mx: 2}} onClick={() => {
-                        loginWithPopup()
-                    }}>
-                        <LoginIcon/>
-                    </IconButton>}
+                {UserStorage.isLogin ? <PersonalMenu/> : <LoginButton/>}
             </Stack>
         </Stack>
     )

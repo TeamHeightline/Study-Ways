b4f573fba00490ee0719fb48a01877b870dd6b0b
timofei <teamheightline@mail.ru>
2025-01-01 21:55:13 +0300

    Общий UI - ширина проекта - убрал горизонтальный overflow

--- src/router-wrapper.tsx
+++ src/router-wrapper.tsx
@@ -2,8 +2,8 @@ import {Box} from "@mui/material";
 
 export function RouterWrapper(props) {
     return (
-        <Box sx={{width: '100vw', display: 'flex', justifyContent: 'center'}}>
-            <Box sx={(theme) => ({flex: 1, width: '100%', maxWidth: theme.breakpoints.values.xl})}>
+        <Box sx={{display: 'flex', justifyContent: 'center'}}>
+            <Box sx={(theme) => ({flex: 1, width: '100%', overflowX: 'none', maxWidth: theme.breakpoints.values.xl})}>
                 {props.children}
             </Box>
         </Box>

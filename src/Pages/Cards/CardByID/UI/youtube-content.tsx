22c226ca066c1ae183d4e0312e1b8b1b7781a639
timofei <teamheightline@mail.ru>
2025-02-02 09:48:06 +0300

    Подготовил модуль карточек к использованию видео с VK

--- src/Pages/Cards/CardByID/UI/youtube-content.tsx
+++ src/Pages/Cards/CardByID/UI/youtube-content.tsx
@@ -13,10 +13,23 @@ interface IYoutubeContentProps extends PaperProps {
 
 }
 
+function getVkVideoIframeUrl(defaultVideoURL: string) {
+    const iframeTransformedURL = defaultVideoURL.split('video')?.[1].split('_')
+    const oid = iframeTransformedURL[0]
+    const id = iframeTransformedURL[1]
+    const iframeVideoURL = `https://vk.com/video_ext.php?oid=${oid}&id=${id}hd=1&autoplay=1`
+    return iframeVideoURL
+}
+
 const YoutubeContent = observer(({card_store, ...props}: IYoutubeContentProps) => {
     const youtubeVideoURL = card_store?.card_data?.videoUrl || ""
     const showVideo = !!youtubeVideoURL
     const isMobile = isMobileHook()
+
+    // TODO когда в редакторе карточек закончу с добавлением типа карточки VK, нужно вернуться к этому месту
+    const vkVideoUrl = 'https://vk.com/video4604580_456240597'
+    const isVkVideo = false
+
     const onEndVideoWatch = () => {
         card_store.isOpenGoToTestDialogAfterVideo = true
     }
@@ -28,15 +41,25 @@ const YoutubeContent = observer(({card_store, ...props}: IYoutubeContentProps) =
             <div style={{position: "relative"}}>
                 <div style={{
                     position: "absolute", left: 0,
-                    right: 0
+                    right: 0,
+                    width: isMobile ? "95vw" : "auto",
+                    height: isMobile ? "53vw" : 540,
+                    display: 'flex'
                 }}>
-                    {showVideo &&
-                        <ReactPlayer width={isMobile ? "95vw" : "auto"}
-                                     height={isMobile ? "53vw" : 540}
-                                     controls
-                                     url={youtubeVideoURL}
-                                     onEnded={onEndVideoWatch}
-                        />
+                    {isVkVideo ?
+                        <iframe src={getVkVideoIframeUrl(vkVideoUrl)}
+                                style={{flex: 1}}
+                                allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+                                frameBorder="0" allowFullScreen></iframe> :
+                        showVideo ?
+                            <ReactPlayer
+                                // style={{flex: 1}}
+                                width={isMobile ? "95vw" : "100%"}
+                                height={isMobile ? "53vw" : 540}
+                                controls
+                                url={youtubeVideoURL}
+                                onEnded={onEndVideoWatch}
+                            /> : null
                     }
                 </div>
                 {card_store.card_data?.isCardUseTestInCard && card_store.isOpenGoToTestDialogAfterVideo && test_after_card_id &&
